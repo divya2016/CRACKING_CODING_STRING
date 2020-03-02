ec92575c0514572e353cd5fc01560ddb4e3e5024
Divya.Rani <divya@credochain.com>
2020-03-02 17:54:14 +0530

    bit vector solution added

--- src/com/practice/isUnique/UsingBitVector.java
+++ src/com/practice/isUnique/UsingBitVector.java
@@ -1,5 +1,32 @@
 package com.practice.isUnique;
 
-public class UsingBitVector {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class UsingBitVector {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		Boolean bool = isUnique(s);
+		if (bool) {
+			System.out.println("All Unique Character");
+		} else {
+			System.out.println("Duplicate Character");
+		}
+		scan.close();
+	}
+
+	private static Boolean isUnique(String s) {
+		int counter=0;
+		char[] arr = s.toCharArray();
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+				int num =arr[i]-'a';
+				if((counter & (1<<num)) >0) {
+					return false;
+				}
+				counter |=1 <<num;
+			}
+		return true;
+	}
 }
--- src/com/practice/isUnique/UsingSorting.java
+++ src/com/practice/isUnique/UsingSorting.java
@@ -26,5 +26,4 @@ public class UsingSorting {
 			}
 		return true;
 	}
-
 }
